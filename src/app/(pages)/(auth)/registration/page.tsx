114cb7e6322af75e3374ade4e0023c0822144084
Anastasia <www.nastya97@yandex.ru>
2024-05-01 16:41:42 +0300

    feat: add button component

--- src/app/(pages)/(auth)/registration/page.tsx
+++ src/app/(pages)/(auth)/registration/page.tsx
@@ -1,7 +1,14 @@
 import "./registration.scss";
 
+import Button from "../../../components/App/button/button";
+
 const RegistrationPage = () => {
-    return <h1>Registration page</h1>;
+    return (
+        <section style={{ border: "1px solid red" }}>
+            <h1>Registration page</h1>
+            <Button title="Зарегистрироваться" />
+        </section>
+    );
 };
 
 export default RegistrationPage;
