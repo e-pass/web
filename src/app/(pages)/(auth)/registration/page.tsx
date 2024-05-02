c55c4c2fa4fc46555e53a5ba5264dd8c4ad2a8e5
Anastasia <www.nastya97@yandex.ru>
2024-05-02 20:11:19 +0300

    feat: checkboxes without size, bg and custom checkmark

--- src/app/(pages)/(auth)/registration/page.tsx
+++ src/app/(pages)/(auth)/registration/page.tsx
@@ -25,6 +25,19 @@ const RegistrationPage = () => {
                 <Input type="tel" placeholder="Номер телефона" />
             </div>
 
+            <div className="registration__checkboxes">
+                <div className="checkbox__container">
+                    <input id="coach" className="checkbox" type="checkbox" />
+                    <label htmlFor="coach">Я — тренер</label>
+                </div>
+                <div className="checkbox__container">
+                    <input id="conversation" className="checkbox" type="checkbox" />
+                    <label htmlFor="conversation">
+                        Соглашаюсь с Политикой обработки персональных данных
+                    </label>
+                </div>
+            </div>
+
             <Button disabled title="Зарегистрироваться" />
         </section>
     );
