00a037d93958375619805a170bd3e5a7eb06aeb0
Anastasia <www.nastya97@yandex.ru>
2024-05-11 18:01:34 +0300

    feat: wrong validation

--- src/app/(pages)/(auth)/registration/page.tsx
+++ src/app/(pages)/(auth)/registration/page.tsx
@@ -3,15 +3,51 @@
 import "./registration.scss";
 
 import Link from "next/link";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 import { Button, Header, Input, PrimaryCheckbox } from "@/components";
+import { IShippingFields } from "@/components/App/primary-input/primary-input";
 
 const RegistrationPage = () => {
     const [isTrainer, setIsTrainer] = useState<boolean>(false);
     const [isAcceptPolicy, setIsAcceptPolicy] = useState<boolean>(false);
+    const { handleSubmit } = useForm<IShippingFields>();
 
-    const formRef = useRef<HTMLFormElement>(null);
+    const onSubmit: SubmitHandler<IShippingFields> = (data) => {
+        console.log(data);
+    };
+    /*  -------------- Тут опции для инпутов ----------------  */
+
+    // const nameRegister = {
+    //     ...register("name", {
+    //         required: "error text",
+    //         minLength: {
+    //             value: 4,
+    //             message: "error message",
+    //         },
+    //     }),
+    // };
+
+    // const emailRegister = {
+    //     ...register("email", {
+    //         required: "error text",
+    //         minLength: {
+    //             value: 8,
+    //             message: "error message",
+    //         },
+    //     }),
+    // };
+
+    // const phoneNumberRegister = {
+    //     ...register("email", {
+    //         required: "error text",
+    //         minLength: {
+    //             value: 11,
+    //             message: "error message",
+    //         },
+    //     }),
+    // };
 
     return (
         <section className="registration">
@@ -25,23 +61,24 @@ const RegistrationPage = () => {
                 </p>
             </Header>
 
-            <form ref={formRef} className="registration__inputs">
-                <Input type="text" placeholder="Имя" />
-                <Input type="text" placeholder="Фамилия" />
-                <Input type="tel" placeholder="Номер телефона" />
+            <form onSubmit={handleSubmit(onSubmit)} className="registration__inputs">
+                <input name="test" type="text" placeholder="test" />
+                <Input name="name" type="text" placeholder="Имя" />
+                <Input name="secondName" type="text" placeholder="Фамилия" />
+                <Input name="phoneNumber" type="tel" placeholder="Номер телефона" />
                 <PrimaryCheckbox
                     text="Я — тренер"
                     isChecked={isTrainer}
                     changeActive={() => setIsTrainer((prevState) => !prevState)}
                 />
                 <PrimaryCheckbox
-                    text="Соглашаюсь с Политикой обработки персональных данных"
+                    text="Соглашаюсь с Политикой обработки персональных данных"
                     isChecked={isAcceptPolicy}
                     changeActive={() => setIsAcceptPolicy((prevState) => !prevState)}
                 />
-            </form>
 
-            <Button disabled={false} title="Зарегистрироваться" />
+                <Button disabled={false} title="Зарегистрироваться" />
+            </form>
         </section>
     );
 };
--- src/components/App/primary-input/primary-input.tsx
+++ src/components/App/primary-input/primary-input.tsx
@@ -3,15 +3,24 @@
 import "./primary-input.scss";
 
 import { ChangeEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+
+export interface IShippingFields {
+    name: string;
+    secondName: string;
+    phoneNumber: string;
+}
 
 interface IInputProps {
     placeholder: string;
     type: string;
+    name: "name" | "secondName" | "phoneNumber";
 }
 
-export const Input = ({ placeholder, type }: IInputProps) => {
+export const Input = ({ placeholder, type, name }: IInputProps) => {
     const [text, setText] = useState<string>("");
     const [focused, setFocused] = useState<boolean>(false);
+    const { register } = useForm<IShippingFields>();
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setText(e.target.value);
@@ -32,6 +41,8 @@ export const Input = ({ placeholder, type }: IInputProps) => {
             <input
                 type={type}
                 id="input"
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...register(name)}
                 className="input"
                 value={text}
                 onChange={handleInputChange}
