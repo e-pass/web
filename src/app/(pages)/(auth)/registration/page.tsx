cac21811b34c5e857bc5ec72398af78561af9c5d
Anastasia <www.nastya97@yandex.ru>
2024-05-12 21:17:43 +0300

    feat: add forwardRef, but form still not work

--- src/app/(pages)/(auth)/registration/page.tsx
+++ src/app/(pages)/(auth)/registration/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 "use client";
 
 import "./registration.scss";
@@ -12,42 +14,11 @@ import { IShippingFields } from "@/components/App/primary-input/primary-input";
 const RegistrationPage = () => {
     const [isTrainer, setIsTrainer] = useState<boolean>(false);
     const [isAcceptPolicy, setIsAcceptPolicy] = useState<boolean>(false);
-    const { handleSubmit } = useForm<IShippingFields>();
+    const { register, handleSubmit } = useForm<IShippingFields>();
 
     const onSubmit: SubmitHandler<IShippingFields> = (data) => {
         console.log(data);
     };
-    /*  -------------- Тут опции для инпутов ----------------  */
-
-    // const nameRegister = {
-    //     ...register("name", {
-    //         required: "error text",
-    //         minLength: {
-    //             value: 4,
-    //             message: "error message",
-    //         },
-    //     }),
-    // };
-
-    // const emailRegister = {
-    //     ...register("email", {
-    //         required: "error text",
-    //         minLength: {
-    //             value: 8,
-    //             message: "error message",
-    //         },
-    //     }),
-    // };
-
-    // const phoneNumberRegister = {
-    //     ...register("email", {
-    //         required: "error text",
-    //         minLength: {
-    //             value: 11,
-    //             message: "error message",
-    //         },
-    //     }),
-    // };
 
     return (
         <section className="registration">
@@ -62,10 +33,28 @@ const RegistrationPage = () => {
             </Header>
 
             <form onSubmit={handleSubmit(onSubmit)} className="registration__inputs">
-                <input name="test" type="text" placeholder="test" />
-                <Input name="name" type="text" placeholder="Имя" />
-                <Input name="secondName" type="text" placeholder="Фамилия" />
-                <Input name="phoneNumber" type="tel" placeholder="Номер телефона" />
+                <Input
+                    {...register("name", {
+                        required: "error message",
+                    })}
+                    type="text"
+                    placeholder="Имя"
+                />
+                <Input
+                    {...register("secondName", {
+                        required: "error message",
+                    })}
+                    type="text"
+                    placeholder="Фамилия"
+                />
+                <Input
+                    {...register("phoneNumber", {
+                        required: "error message",
+                    })}
+                    type="tel"
+                    placeholder="Номер телефона"
+                />
+
                 <PrimaryCheckbox
                     text="Я — тренер"
                     isChecked={isTrainer}
--- src/components/App/primary-input/primary-input.tsx
+++ src/components/App/primary-input/primary-input.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 "use client";
 
 import "./primary-input.scss";
 
-import { ChangeEvent, useState } from "react";
-import { useForm } from "react-hook-form";
+import { ChangeEvent, ForwardedRef, forwardRef, useState } from "react";
 
 export interface IShippingFields {
     name: string;
@@ -17,46 +18,46 @@ interface IInputProps {
     name: "name" | "secondName" | "phoneNumber";
 }
 
-export const Input = ({ placeholder, type, name }: IInputProps) => {
-    const [text, setText] = useState<string>("");
-    const [focused, setFocused] = useState<boolean>(false);
-    const { register } = useForm<IShippingFields>();
+export const Input = forwardRef(
+    (props: IInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+        const [text, setText] = useState<string>("");
+        const [focused, setFocused] = useState<boolean>(false);
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setText(e.target.value);
-    };
+        const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+            setText(e.target.value);
+        };
 
-    const handleFocus = () => {
-        setFocused(true);
-    };
+        const handleFocus = () => {
+            setFocused(true);
+        };
 
-    const handleBlur = () => {
-        if (text === "") {
-            setFocused(false);
-        }
-    };
+        const handleBlur = () => {
+            if (text === "") {
+                setFocused(false);
+            }
+        };
 
-    return (
-        <div className="input-container">
-            <input
-                type={type}
-                id="input"
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...register(name)}
-                className="input"
-                value={text}
-                onChange={handleInputChange}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-            />
+        return (
+            <div className="input-container">
+                <input
+                    id="input"
+                    className="input"
+                    value={text}
+                    onChange={handleInputChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    {...props}
+                    ref={ref}
+                />
 
-            <label
-                htmlFor="input"
-                className={
-                    focused || text ? "placeholder placeholder_active" : "placeholder"
-                }>
-                {placeholder}
-            </label>
-        </div>
-    );
-};
+                <label
+                    htmlFor="input"
+                    className={
+                        focused || text ? "placeholder placeholder_active" : "placeholder"
+                    }>
+                    {props.placeholder}
+                </label>
+            </div>
+        );
+    }
+);
